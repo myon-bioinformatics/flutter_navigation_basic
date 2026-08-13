626e8ce85ca31482aeded4728bb09c797ccf07aa
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-13 10:05:29 +0900

    Update semantic route tests

--- test/core/route_names_test.dart
+++ test/core/route_names_test.dart
@@ -7,16 +7,16 @@ void main() {
       expect(RouteNames.home, equals('/'));
     });
 
-    test('screen2 route is /screen2', () {
-      expect(RouteNames.screen2, equals('/screen2'));
+    test('counter playground keeps canonical /screen2 path', () {
+      expect(RouteNames.counterPlayground, equals('/screen2'));
     });
 
-    test('screen3 route is /screen3', () {
-      expect(RouteNames.screen3, equals('/screen3'));
+    test('irony generator keeps canonical /screen3 path', () {
+      expect(RouteNames.ironyGenerator, equals('/screen3'));
     });
 
-    test('screen4 route is /screen4', () {
-      expect(RouteNames.screen4, equals('/screen4'));
+    test('composition generator keeps canonical /screen4 path', () {
+      expect(RouteNames.compositionGenerator, equals('/screen4'));
     });
   });
 }
