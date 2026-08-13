6a15eecf115f475902b066d2a9bc6258872255d5
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-13 10:02:51 +0900

    Use semantic route names for feature demos

--- lib/core/navigation/route_names.dart
+++ lib/core/navigation/route_names.dart
@@ -2,8 +2,8 @@ class RouteNames {
   RouteNames._();
 
   static const String home = '/';
-  static const String screen2 = '/screen2';
-  static const String screen3 = '/screen3';
-  static const String screen4 = '/screen4';
+  static const String counterPlayground = '/screen2';
+  static const String ironyGenerator = '/screen3';
+  static const String compositionGenerator = '/screen4';
   static const String screen5 = '/screen5';
 }
