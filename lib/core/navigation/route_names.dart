9e7766117bda3155a6e5a92e7c85d4c6076e1834
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-13 10:06:59 +0900

    Move semantic demos under examples paths

--- lib/core/navigation/route_names.dart
+++ lib/core/navigation/route_names.dart
@@ -2,8 +2,8 @@ class RouteNames {
   RouteNames._();
 
   static const String home = '/';
-  static const String counterPlayground = '/screen2';
-  static const String ironyGenerator = '/screen3';
-  static const String compositionGenerator = '/screen4';
+  static const String counterPlayground = '/examples/counter-playground';
+  static const String ironyGenerator = '/examples/irony-generator';
+  static const String compositionGenerator = '/examples/composition-generator';
   static const String screen5 = '/screen5';
 }
