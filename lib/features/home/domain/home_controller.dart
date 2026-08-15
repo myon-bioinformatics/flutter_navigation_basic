c4b280a6b7bc460c93a2928addfb02cdab17b6de
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-16 08:45:45 +0900

    fix: remove legacy intl usage from home controller

--- lib/features/home/domain/home_controller.dart
+++ lib/features/home/domain/home_controller.dart
@@ -1,6 +1,12 @@
-import 'package:intl/intl.dart';
 import '../../../core/services/base_controller.dart';
 
 class HomeController extends BaseController {
-  String get today => DateFormat('yyyy/MM/dd(E)').format(DateTime.now());
+  String get today {
+    const weekdays = <String>['月', '火', '水', '木', '金', '土', '日'];
+    final value = DateTime.now();
+    final month = value.month.toString().padLeft(2, '0');
+    final day = value.day.toString().padLeft(2, '0');
+    final weekday = weekdays[value.weekday - 1];
+    return '${value.year}/$month/$day($weekday)';
+  }
 }
