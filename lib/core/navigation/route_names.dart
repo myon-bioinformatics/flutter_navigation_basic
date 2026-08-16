c2f4a166a7a96d0de5506146af6986b8c587d319
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-16 21:50:20 +0900

    feat: add Clipboard Shelf route name

--- lib/core/navigation/route_names.dart
+++ lib/core/navigation/route_names.dart
@@ -2,6 +2,7 @@ class RouteNames {
   RouteNames._();
 
   static const String home = '/';
+  static const String clipboardShelf = '/core/clipboard-shelf';
   static const String counterPlayground = '/examples/counter-playground';
   static const String ironyGenerator = '/examples/irony-generator';
   static const String compositionGenerator = '/examples/composition-generator';
