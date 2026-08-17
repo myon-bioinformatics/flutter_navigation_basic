e67f963e102c48f57a0a0b2c905eeee667d3cf6d
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-17 21:02:27 +0900

    feat: add reference Now Timeline wrapper

---
+++ lib/screens/now_timeline_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../features/now_timeline/presentation/now_timeline_page.dart';
+
+class NowTimelineScreen extends StatelessWidget {
+  const NowTimelineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const NowTimelinePage();
+}
