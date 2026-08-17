479c10757cb3485b27189d7815579950742e13e2
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-17 22:55:24 +0900

    add reference coordinate screen

---
+++ lib/screens/coordinate_tool_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../features/coordinate_tool/presentation/coordinate_tool_page.dart';
+
+class CoordinateToolScreen extends StatelessWidget {
+  const CoordinateToolScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const CoordinateToolPage();
+}
