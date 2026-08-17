092f00ef28494a56d99b9675e7442b567e59c32b
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-17 22:53:44 +0900

    register coordinate tool route

--- lib/core/navigation/route_names.dart
+++ lib/core/navigation/route_names.dart
@@ -4,6 +4,7 @@ class RouteNames {
   static const String home = '/';
   static const String clipboardShelf = '/core/clipboard-shelf';
   static const String nowTimeline = '/tools/time/now-timeline';
+  static const String coordinateTool = '/tools/location/coordinates';
   static const String counterPlayground = '/examples/counter-playground';
   static const String ironyGenerator = '/examples/irony-generator';
   static const String compositionGenerator = '/examples/composition-generator';
