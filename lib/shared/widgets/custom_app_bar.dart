636b6685bcd2d7482d81efb0ca70f05c7711c16a
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-18 08:40:40 +0900

    add global locale picker to production app bars

--- lib/shared/widgets/custom_app_bar.dart
+++ lib/shared/widgets/custom_app_bar.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import '../display/display_scope.dart';
 import '../themes/color_constants.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key, required this.title});
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.actions = const [],
+    this.showLocalePicker = true,
+  });
 
   final String title;
+  final List<Widget> actions;
+  final bool showLocalePicker;
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -13,6 +21,11 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title),
+      actions: [
+        ...actions,
+        if (showLocalePicker && DisplayScope.maybeOf(context) != null)
+          const DisplayLocalePicker(compact: true),
+      ],
       backgroundColor: ColorConstants.primary,
       foregroundColor: Colors.white,
       elevation: 2,
