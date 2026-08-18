e9d6e6d3ab960f424ded5645f891d3cfd42a6013
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-18 14:53:58 +0900

    feat: expose locale picker from shared app bar

--- lib/shared/widgets/custom_app_bar.dart
+++ lib/shared/widgets/custom_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../display/display_scope.dart';
 import '../themes/color_constants.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -6,10 +7,12 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
     super.key,
     required this.title,
     this.actions = const [],
+    this.showLocalePicker = true,
   });
 
   final String title;
   final List<Widget> actions;
+  final bool showLocalePicker;
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -18,7 +21,11 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title),
-      actions: actions,
+      actions: [
+        ...actions,
+        if (showLocalePicker && DisplayScope.maybeOf(context) != null)
+          const DisplayLocalePicker(compact: true),
+      ],
       backgroundColor: ColorConstants.primary,
       foregroundColor: Colors.white,
       elevation: 2,
