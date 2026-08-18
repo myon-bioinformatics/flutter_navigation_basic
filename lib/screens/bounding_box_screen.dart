2bd21f9e8f789dcd919ed0fd978d78f84429abfe
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-18 12:42:57 +0900

    feat: add bounding box screen wrapper

---
+++ lib/screens/bounding_box_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import '../features/bounding_box/presentation/bounding_box_page.dart';
+
+class BoundingBoxScreen extends StatelessWidget {
+  const BoundingBoxScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const BoundingBoxPage();
+}
