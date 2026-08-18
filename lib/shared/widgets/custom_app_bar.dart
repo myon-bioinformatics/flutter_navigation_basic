3157f544fe65d46246c5103e8a913d25ea2f41d1
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-18 13:12:06 +0900

    refactor: allow CustomAppBar actions

--- lib/shared/widgets/custom_app_bar.dart
+++ lib/shared/widgets/custom_app_bar.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import '../themes/color_constants.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({super.key, required this.title});
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.actions = const [],
+  });
 
   final String title;
+  final List<Widget> actions;
 
   @override
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
@@ -13,6 +18,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Widget build(BuildContext context) {
     return AppBar(
       title: Text(title),
+      actions: actions,
       backgroundColor: ColorConstants.primary,
       foregroundColor: Colors.white,
       elevation: 2,
