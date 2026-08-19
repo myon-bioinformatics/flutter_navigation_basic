f68186fb478b0b53d2e4311e98155a7ac57117dc
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-08-19 19:22:45 +0900

    test: cover coordinate map URLs

--- test/coordinate_tool/coordinate_formatter_test.dart
+++ test/coordinate_tool/coordinate_formatter_test.dart
@@ -98,6 +98,13 @@ void main() {
       expect(longitudeHalfSpan, greaterThan(latitudeHalfSpan));
       expect(area.centerRadiusText, contains('radius_m: 100'));
       expect(area.jsonText, contains('"wraps_antimeridian": false'));
+      expect(
+        area.googleMapsUri.toString(),
+        contains('https://www.google.com/maps/search/?api=1'),
+      );
+      expect(area.googleMapsUri.toString(), contains('35.681236%2C139.767125'));
+      expect(area.appleMapsUri.toString(), contains('https://maps.apple.com/?'));
+      expect(area.appleMapsUri.toString(), contains('ll=35.681236%2C139.767125'));
     });
 
     test('emits directly usable Google Maps and Apple MapKit circle snippets', () {
