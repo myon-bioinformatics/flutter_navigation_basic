1317314f981084913d1804cbb76a79172b4eecdd
myon <54614626+myon-bioinformatics@users.noreply.github.com>
2026-09-26 12:53:33 +0900

    test(a11y): lock restored Back to Hub semantics

--- test/screens/generic_screen_test.dart
+++ test/screens/generic_screen_test.dart
@@ -73,15 +73,12 @@ void main() {
       final node = tester.getSemantics(find.byKey(const Key('back-to-hub')));
       final data = node.getSemanticsData();
       expect(node.identifier, 'back-to-hub');
-      debugPrint(
-        '[back-to-hub a11y] actions=${data.actions} '
-        'button=${data.hasFlag(SemanticsFlag.isButton)} '
-        'hasEnabledState=${data.hasFlag(SemanticsFlag.hasEnabledState)} '
-        'enabled=${data.hasFlag(SemanticsFlag.isEnabled)} '
-        'focusable=${data.hasFlag(SemanticsFlag.isFocusable)} '
-        'focusAction=${data.hasAction(SemanticsAction.focus)}',
-      );
+      expect(data.hasFlag(SemanticsFlag.isButton), isTrue);
+      expect(data.hasFlag(SemanticsFlag.hasEnabledState), isTrue);
+      expect(data.hasFlag(SemanticsFlag.isEnabled), isTrue);
+      expect(data.hasFlag(SemanticsFlag.isFocusable), isTrue);
       expect(data.hasAction(SemanticsAction.tap), isTrue);
+      expect(data.hasAction(SemanticsAction.focus), isTrue);
       expect(node.label, 'Back to Hub');
       var children = 0;
       node.visitChildren((_) {
